ebd28c9deac003da022b82072e85652b8e89f910
joseaznar <jose.aznar94@gmail.com>
2017-08-10 19:43:14 -0500

    Modify the query for twitter and watson to generalyze it

--- crazy_ai_api/modules/twitter/views.py
+++ crazy_ai_api/modules/twitter/views.py
@@ -1,19 +1,21 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status 
+from rest_framework import status
 from django.http import Http404
 import json
 import twitter
 from twitter import TwitterError
 
-from .serializers import  TweetSerializer
+from .serializers import TweetSerializer
 
 # Create your views here.
+
+
 class ListTweets(APIView):
 
-    def get(self,request, query):
-    	# http://127.0.0.1:8000/api/v1/tweets/(thing to look for)/
+    def get(self, request, query):
+        # http://127.0.0.1:8000/api/v1/tweets/(thing to look for)/
         api = twitter.Api(
             base_url='https://api.twitter.com/1.1',
             consumer_key='QmMcxaV3mA4pkxiUFtCk5gu85',
@@ -23,11 +25,11 @@ class ListTweets(APIView):
         )
 
         results = api.GetSearch(
-        raw_query="q=" + query)	
+            raw_query="q=" + query)
 
         data = []
         for stat in results:
-        	data.append(stat._json)
+            data.append(stat._json)
 
-        return Response(data,status=status.HTTP_200_OK)
-        #return Response(results[0]._json,status=status.HTTP_200_OK)	
+        return Response(data, status=status.HTTP_200_OK)
+        # return Response(results[0]._json,status=status.HTTP_200_OK)
--- crazy_ai_api/modules/watson/views.py
+++ crazy_ai_api/modules/watson/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status 
+from rest_framework import status
 from django.http import Http404
 import json
 import twitter
@@ -9,10 +9,12 @@ from twitter import TwitterError
 from watson_developer_cloud import ToneAnalyzerV3
 
 # Create your views here.
+
+
 class ProcessTweets(APIView):
 
-    def get(self,request, query):
-    	# http://127.0.0.1:8000/api/v1/tweets/(thing to look for)/
+    def get(self, request, query):
+        # http://127.0.0.1:8000/api/v1/tweets/(thing to look for)/
         api = twitter.Api(
             base_url='https://api.twitter.com/1.1',
             consumer_key='QmMcxaV3mA4pkxiUFtCk5gu85',
@@ -22,18 +24,18 @@ class ProcessTweets(APIView):
         )
 
         results = api.GetSearch(
-        raw_query="q=" + query)	
+            raw_query="q=" + query)
 
         data = []
         for stat in results:
-        	data.append(stat._json)
+            data.append(stat._json)
 
         tone_analyzer = ToneAnalyzerV3(
             version='2016-05-19',
             username='0b3bb1e5-5aff-422c-b4a7-75463109dc2d',
             password='UwzifwOO3iCQ'
         )
-        
+
         texto = ""
 
         for dat in data:
@@ -41,5 +43,5 @@ class ProcessTweets(APIView):
 
         #tone = tone_analyzer.tone(texto, content_type='application/json')
         tone = tone_analyzer.tone(texto, content_type='text/plain')
-        return Response(tone,status=status.HTTP_200_OK)
-        #return Response(results[0]._json,status=status.HTTP_200_OK)	
+        return Response(tone, status=status.HTTP_200_OK)
+        # return Response(results[0]._json,status=status.HTTP_200_OK)
