5c1890af0eda85324d3208f5d2fb839b5341a32b
Alexandro Sánchez Rios <alexrios.asrios@gmail.com>
2017-08-14 19:43:43 -0500

    add changes to analyse button

--- crazy_ai_api/crazy_ai_api/settings.py
+++ crazy_ai_api/crazy_ai_api/settings.py
@@ -77,14 +77,13 @@ WSGI_APPLICATION = 'crazy_ai_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        #'NAME': 'db_goodreads',
-        #'USER': 'admin_goodreads',
-        #'PASSWORD':'goodreads2017',
-        #'HOST':'localhost',
-        #'PORT':'5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
